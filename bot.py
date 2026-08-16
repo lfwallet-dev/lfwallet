d5727345e2198aea06ed72eb31d683de7432dfcf
lfwallet-dev <amakusa0x@gmail.com>
2026-08-16 16:17:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ MENU_BUTTON_1_TEXT = "🚀 LF Wallet"
 MENU_BUTTON_1_URL = "https://t.me/LFWallet_AirdropBot?start=ref8994710600"
 
 MENU_BUTTON_2_TEXT = "🐶 DoggieZen"
-MENU_BUTTON_2_URL = "https://t.me/doggiezenbot/DoggieZen?startapp=4UQKTU8"
+MENU_BUTTON_2_URL = "https://t.me/doggiezen"
 
 # Message automatically sent when someone sends a private message (DM) to the bot.
 DM_REPLY_MESSAGE = (
